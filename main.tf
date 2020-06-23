a80847e4bb828025cefa542db9c79be3827b28fb
Brian Fitzwater - IDI-C <brian.fitzwater@gsa.gov>
2020-06-23 09:54:06 -0400

    changes for testing

--- main.tf
+++ main.tf
@@ -4,6 +4,5 @@ data "aws_caller_identity" "current" {}
 locals {
   app_name       = "${var.project}-${var.appenv}-ansible-lambda"
   account_id     = data.aws_caller_identity.current.account_id
-  lambda_src     = "release/grace-ansible-lambda.zip"
   lambda_handler = "grace-ansible-lambda"
 }
