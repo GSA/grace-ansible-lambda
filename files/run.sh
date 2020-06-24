463ece6de74f3512f0c2e034f9d1d31e221fa794
Bryan Alexander <bryan.alexander@gsa.gov>
2020-06-24 14:48:16 -0500

    updates run.sh removing cloudwatch agent as the url was inaccessible

--- files/run.sh
+++ files/run.sh
@@ -4,9 +4,6 @@ apt-get update
 apt-get install -y python-pip
 pip install awscli
 
-wget https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
-rpm -U ./amazon-cloudwatch-agent.rpm
-
 aws s3 cp s3://${bucket}/files/runner.py /tmp/runner.py
 chmod +x /tmp/runner.py
 
