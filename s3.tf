96993717a32f09065ffb440a85e4ea444888836a
Bryan Alexander <bryan.alexander@gsa.gov>
2020-06-28 02:04:50 -0500

    adds -u ec2-user

--- s3.tf
+++ s3.tf
@@ -50,6 +50,7 @@ resource "aws_s3_bucket_object" "user_data" {
     hosts_file = join("/", ["ansible", var.appenv])
     site_file  = "ansible/site.yml"
     key_file   = var.key_file
+    ec2_user   = var.ec2_user
   })
   kms_key_id = aws_kms_key.kms.arn
 }
