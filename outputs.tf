7df02a5e27c9e98200e3e009151afe17e0c33fda
Bryan Alexander <bryan.alexander@gsa.gov>
2020-07-27 11:39:09 -0500

    adds s3_bucket_arn output variable

--- outputs.tf
+++ outputs.tf
@@ -13,6 +13,9 @@ output "kms_key_alias" {
 output "s3_bucket" {
   value = aws_s3_bucket.bucket.id
 }
+output "s3_bucket_arn" {
+  value = aws_s3_bucket.bucket.arn
+}
 output "lambda_arn" {
   value = aws_lambda_function.lambda.arn
 }
