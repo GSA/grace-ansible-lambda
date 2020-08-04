6948b3edc7146ac2f2e8a58b66f577a4eafd7a2b
Bryan Alexander <bryan.alexander@gsa.gov>
2020-08-04 14:26:40 -0500

    terraform fmt

--- events.tf
+++ events.tf
@@ -12,6 +12,6 @@ EOF
 }
 
 resource "aws_cloudwatch_event_target" "ansible" {
-  rule  = aws_cloudwatch_event_rule.ansible.name
-  arn   = aws_cloudwatch_log_group.ansible.arn
+  rule = aws_cloudwatch_event_rule.ansible.name
+  arn  = aws_cloudwatch_log_group.ansible.arn
 }
